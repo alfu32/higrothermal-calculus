1dcb58034f29e720a44efa7df81d2ae2e7bc4d8f
alfu32 <ioan.alferaru@gmail.com>
2017-12-28 07:59:35 +0100

    heath transfer process model source url

--- src/app/HeathTransferProcessModel.ts
+++ src/app/HeathTransferProcessModel.ts
@@ -1,17 +1,19 @@
 import { Layer } from "./Layer";
 export class HeathTransferProcessModel {
     /**
+    * @source
+https://www.researchgate.net/
+publication/280839124_Effect_of_flow_pulsation_on_energy_consumption_of_a_radiator_in_a_centrally_heated_building
     * Q = U*A*deltaT
     * U = 1/ Rtotal
     * Rtotal = RAIndoor + sum(0,N,RLayer[i]) + RAOutdoor
     * Rtotal = 1/alphaIndoor + sum(0,N,layer[i].width/layer[i].k) + 1/alphaOutdoor
     */
     public static getTemperaturesForLayers(layers: Layer[]) {
-        return layers.reduce( (acc, layer, index, _layers) => {
-            if ( index === 0 ) {
-
+        return layers.reduce((acc, layer, index, _layers) => {
+            if (index === 0) {
             }
             return acc;
-        }, [])
+        }, []);
     }
 }
